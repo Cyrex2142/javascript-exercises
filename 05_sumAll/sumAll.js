ab89ce30f22beee7afc22cdb383129ba264f45af
Cyrex2142 <93499531+Cyrex2142@users.noreply.github.com>
2022-04-19 12:12:31 +0930

    Exercise 5
    Messy code needs polishing will circle back later

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,26 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
+    let newInt = 0;
+    if (int1 < 0 || int2 < 0) {
+        return 'ERROR'
+    } else if (typeof int1 != "number"|| typeof int2 != "number"){
+        return 'ERROR'
+    } else if(int1 < int2) {
+        var intCounter = int1;
+        var highInt = int2;
+        while(intCounter <= highInt) {
+            newInt = newInt + intCounter;
+            intCounter ++;
+        }
+        return newInt;
+    } else if(int1 > int2) {
+        var intCounter = int2;
+        var highInt = int1;
+        while(intCounter <= highInt) {
+            newInt = newInt + intCounter;
+            intCounter ++;
+        }
+        return newInt;
+    }
 
 };
 
