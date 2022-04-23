9163bd417cd1d3ed8a9a8b9cc044d4573e90a90b
Cyrex2142 <93499531+Cyrex2142@users.noreply.github.com>
2022-04-23 19:02:40 +0930

    Temp and Calc exercises 100% pass tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(farDeg) {
+  let celsiusTemp = 0;
+  celsiusTemp = Math.round(((farDeg - 32) * 5/9) * 10) / 10;
+  return celsiusTemp;
 };
 
-const ctof = function() {
-
+const ctof = function(celDeg) {
+  let farenheitTemp = 0;
+  farenheitTemp = Math.round(((celDeg * 1.8) + 32) * 10) / 10;
+  return farenheitTemp;
 };
 
 // Do not edit below this line
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return Math.floor(num1 + num2);
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numArr) {
+  let outNum = 0;
+	for (let i = 0; i < numArr.length; i++) {
+    outNum += numArr[i];
+  }
+  return outNum;
 };
 
-const multiply = function() {
-
+const multiply = function(numArr) {
+  let outNum = numArr[0] * numArr[1];
+  for (let i= 2; i < numArr.length; i++) {
+    outNum = outNum*numArr[i];
+  }
+  return outNum;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(facNum) {
+	let i = 1;
+  let numArr = [];
+  let outNum = 1;
+  while (i <= facNum) {
+    numArr.push(i);
+    i++;
+  }
+  for (let x = 1; x < numArr.length; x++) {
+    outNum = outNum*numArr[x];
+  }
+  return outNum;
 };
 
 // Do not edit below this line
