a87cd592591a477edccadeddf4ec8d628dbe2581
Cyrex2142 <93499531+Cyrex2142@users.noreply.github.com>
2022-04-25 11:28:03 +0930

    started palidromes, fibonacci and get the Oldest, completed get the titles

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,5 +1,13 @@
-const caesar = function() {
-
+const caesar = function(str, key) {
+    let crypt = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    let swapKey = '';
+    for (let i = 0; i < key; i++) {
+        swapKey = crypt.pop();
+        crypt.unshift(swapKey);
+    }
+    for (let x = 0; x < str.length; x++) {
+        
+    }
 };
 
 // Do not edit below this line
